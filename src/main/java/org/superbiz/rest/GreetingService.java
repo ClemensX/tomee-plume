2f843461ff11fa8ea5f9e6251c1603b93a9d28be
Clemens Fehr <clemensfehr@gmx.de>
2018-08-26 18:29:04 +0200

    allow manual switch to hsqldb in resources.xml

--- src/main/java/org/superbiz/rest/GreetingService.java
+++ src/main/java/org/superbiz/rest/GreetingService.java
@@ -3,16 +3,19 @@ package org.superbiz.rest;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 @Path("/greeting")
 public class GreetingService {
 
 	@GET
+	@Produces("text/plain")
 	public String message() {
 		return "Hi Rest!";
 	}
 	
 	@POST
+	@Produces("text/plain")
 	public String lowerCaseMessage() {
 		return "Hi Rest!".toLowerCase();
 		
